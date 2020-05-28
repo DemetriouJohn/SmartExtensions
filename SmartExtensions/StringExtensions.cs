4d89fb32520fd426b760d710b231d77c960bd25c
john <john.demetriou.jd@gmail.com>
2020-05-28 16:34:23 +0300

    Add StringExtensions

---
+++ SmartExtensions/StringExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace SmartExtensions
+{
+    public static class StringExtensions
+    {
+        public static bool CaseInsensitiveContains(this string first, string second)
+        {
+            return second.IsNullOrEmpty() || first.IndexOf(second, StringComparison.InvariantCultureIgnoreCase) >= 0;
+        }
+
+        public static bool CaseInsensitiveCompare(this string first, string second)
+        {
+            return string.Compare(first, second, StringComparison.InvariantCultureIgnoreCase) == 0;
+        }
+
+        public static string ToValueOrDefault(this string value, string @default)
+        {
+            return !string.IsNullOrEmpty(value) ? value : @default;
+        }
+
+        public static bool IsNullOrEmpty(this string first)
+        {
+            return string.IsNullOrEmpty(first);
+        }
+
+        public static string RemoveSpaces(this string value)
+        {
+            return value.Replace(" ", string.Empty);
+        }
+
+        public static bool TryToInt(this string input, out int result)
+        {
+            return int.TryParse(input, out result);
+        }
+
+        public static bool TryToDate(this string input, out DateTime result)
+        {
+            return DateTime.TryParse(input, out result);
+        }
+    }
+}
