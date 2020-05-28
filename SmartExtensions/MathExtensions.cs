15e64bfeb619f240c47ee6487cb77d46ddedc984
john <john.demetriou.jd@gmail.com>
2020-05-28 16:52:44 +0300

    Add MathExtensions

---
+++ SmartExtensions/MathExtensions.cs
@@ -0,0 +1,40 @@
+using System;
+
+namespace SmartExtensions
+{
+    public static class MathExtensions
+    {
+        public static double Pow(this double baseNum, byte power)
+        {
+            double result = 1;
+            for (var i = 0; i < power; i++)
+            {
+                result *= baseNum;
+            }
+
+            return result;
+        }
+
+        public static double Pow(this double baseNum, ushort power)
+        {
+            double result = 1;
+            for (var i = 0; i < power; i++)
+            {
+                result *= baseNum;
+            }
+
+            return result;
+        }
+
+         public static double Pow(this double baseNum, uint power)
+        {
+            double result = 1;
+            for (var i = 0; i < power; i++)
+            {
+                result *= baseNum;
+            }
+
+            return result;
+        }
+    }
+}
